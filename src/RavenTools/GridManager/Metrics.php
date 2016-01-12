aa061bc47385d967da7054b6d9e89fb33234b98a
Phil Dier <phil@raventools.com>
2016-01-12 10:51:18 -0600

    switch metrics to 3rd party client

--- src/RavenTools/GridManager/Metrics.php
+++ src/RavenTools/GridManager/Metrics.php
@@ -2,23 +2,46 @@
 
 namespace RavenTools\GridManager;
 
+use \Domnikl\Statsd\Client as StatsD;
+use \Domnikl\Statsd\Connection\UdpSocket;
+
 class Metrics {
 
-	private $prefix = null;
+	private $client = null;
 
-	public function __construct($prefix) {
-		$this->prefix = $prefix;
-	}
+	public function __construct($params) {
 
-	private function key($suffix) {
-		return sprintf("%s.%s",$this->prefix,$suffix);
+		if(array_key_exists('prefix',$params) && !empty($params['prefix'])) {
+			$prefix = $params['prefix'];
+		} else {
+			throw new \Exception("prefix parameter required");
+		}
+
+		$host = "127.0.0.1";
+		if(array_key_exists('host',$params) && !empty($params['host'])) {
+			$host = $params['host'];
+		}
+
+		$port = 8125;
+		if(array_key_exists('port',$params) && !empty($params['port'])) {
+			$port = (int)$params['port'];
+		}
+
+		if(array_key_exists('client',$params) && !empty($params['client'])) {
+			$this->client = $params['client'];
+		} else {
+			$this->client = new StatsD(
+				new UdpSocket($host,$port),
+				$prefix
+			);
+		}
 	}
 
 	public function increment($suffix,$sample=1,$value=1) {
-		\StatsD::increment($this->key($suffix),$sample,$value);
+		$this->client->count($suffix,$value,$sample);
 	}
 
 	public function gauge($suffix,$value) {
-		\StatsD::gauge($this->key($suffix),$value);
+		$this->client->gauge($suffix,$value);
 	}
 }
